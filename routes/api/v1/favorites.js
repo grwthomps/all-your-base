a2164045c1599466b8c7358350e14fa2f819a7c8
Graham Thompson <grwthomps@gmail.com>
2020-01-30 10:39:37 -0700

    Add favorites db insert and implement sad paths for bad request or credentials

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -10,11 +10,14 @@ router.post('/', function (req, res) {
     .first()
     .then(user => {
       if (user) {
-        console.log(user)
+        database('favorites').insert({user_id: user.id, location: req.body.location}, 'id')
+          .then(id => {
+            res.status(201).json({success: `${req.body.location} favorite has been created.`})
+          }).catch(error => console.log('DB insert error'))
       } else {
-        res.status(401).json({error: 'Bad credentials.'});
+        res.status(401).json({error: 'Unauthorized'});
       }
-  }).catch(error => console.log(error))
+  }).catch(error => res.status(400).json({error: 'Bad request.'}))
 })
 
 module.exports = router;
