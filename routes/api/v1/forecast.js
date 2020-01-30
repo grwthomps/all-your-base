a6f4c39d996a469d99ba1d941d83b3deaaa5a649
Graham Thompson <grwthomps@gmail.com>
2020-01-30 12:40:06 -0700

    Add dark sky api call to forecast route and verify functional

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -13,7 +13,11 @@ router.get('/', function (req, res) {
       if (user) {
         fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.location}&key=${process.env.GOOGLE_API_KEY}`)
           .then(google_res => google_res.json())
-          .then(google_res => res.status(200).json(google_res))
+          .then(google_res => {
+            fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${google_res.results[0].geometry.location.lat},${google_res.results[0].geometry.location.lng}`)
+              .then(darksky_res => darksky_res.json())
+              .then(darksky_res => res.status(200).json(darksky_res))
+          })
       } else {
         res.status(401).json({error: 'Unauthorized'});
       }
