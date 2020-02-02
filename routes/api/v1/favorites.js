56545cda1344e71bc4d7847506d9d76e351e040b
Graham Thompson <grwthomps@gmail.com>
2020-02-02 13:37:46 -0700

    Add endpoint for listing favorites to favorites route file

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
+const Forecast = require('../../../pojos/forecast')
 
 const enviroment = process.env.NODE_ENV || 'development'
 const config = require("../../../knexfile")[enviroment]
@@ -14,6 +16,32 @@ router.post('/', function (req, res) {
           .then(id => {
             res.status(201).json({success: `${req.body.location} favorite has been created.`})
           }).catch(error => console.log('DB insert error'))
+      } else {
+        res.status(401).json({error: 'Unauthorized'});
+      }
+  }).catch(error => res.status(400).json({error: 'Bad request.'}))
+})
+
+router.get('/', function (req, res) {
+  database('users').where('api_key', req.body.api_key)
+    .first()
+    .then(user => {
+      if (user) {
+        database('favorites').where('user_id', user.id)
+          .then(favorites => {
+            let favorites_forecasts = favorites.map(favorite => {
+              return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${favorite.location}&key=${process.env.GOOGLE_API_KEY}`)
+                .then(google_res => google_res.json())
+                .then(google_res => {
+                  return fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${google_res.results[0].geometry.location.lat},${google_res.results[0].geometry.location.lng}`)
+                    .then(darksky_res => darksky_res.json())
+                    .then(darksky_res => new Forecast(favorite.location, darksky_res).favoriteForecast())
+                })
+              return favorite
+            })
+            Promise.all(favorites_forecasts)
+              .then(favorites_res => res.status(200).json(favorites_res))
+          })
       } else {
         res.status(401).json({error: 'Unauthorized'});
       }
